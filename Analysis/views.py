bf902ed6f520c9ca3a5dd3fb5c5941e5440f43fe
Akaash C <68701225+akaash198@users.noreply.github.com>
2023-10-24 12:22:24 +0530

    Update views.py

--- Analysis/views.py
+++ Analysis/views.py
@@ -554,7 +554,7 @@ def home(request):
     # print(data)
     # file = FileUpload.objects.all()
     # data = pd.read_csv(file[0].file)
-    data = pd.read_csv("static\data.csv")
+    data = pd.read_csv("data.csv")
     data['Timestamp'] = pd.to_datetime(data['Timestamp'])
     data['Hour'] = data['Timestamp'].dt.hour
     data['Date'] = data['Timestamp'].dt.date
